3d4351fa86298473194a91320e1d05344c66a649
Alejandro Xavier Goncalves <alejandroxavier1121@gmail.com>
2025-08-20 13:12:40 -0300

    Fix rutas de imágenes para GitHub Pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <section id='about' className='container mx-auto py-20 sm:py-28 md:py-36 lg:py-40 2xl:py-48 flex flex-col justify-center items-center' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-      <h1 className='text-3xl font-bold sm:text-4xl 2xl:text-5xl'>Sobre mi</h1>
+      <h1 className='text-3xl font-bold sm:text-4xl 2xl:text-5xl'>Sobre mí</h1>
       <div className='md:flex md:justify-between md:items-start'>
         <p className='about-me my-5'>Mi nombre es Alejandro Xavier, tengo 24 años y 
           resido en Salto, Uruguay. Cuento con la carrera de Analista Programador 
