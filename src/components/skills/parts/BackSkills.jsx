037d2372ff118ad253c3a938d093cab1414212a7
Alejandro Xavier Goncalves <alejandroxavier1121@gmail.com>
2025-08-20 14:07:46 -0300

    Index ok + ajustes

--- src/components/skills/parts/BackSkills.jsx
+++ src/components/skills/parts/BackSkills.jsx
@@ -1,52 +1,60 @@
-import React from 'react'
-import PHP from 'assets/svg/php.svg'
-import Node from 'assets/svg/node.svg'
-import csharp from 'assets/svg/csharp.svg'
-import MySQL from 'assets/svg/mysql.svg'
-import PostgreSQL from 'assets/svg/postgresql.svg'
-import MongoDB from 'assets/svg/mongodb.svg'
+import React from "react";
+import { asset } from "../../../lib/asset"; 
+
+// íconos 
+const PHP         = asset("assets/svg/php.svg");
+const Node        = asset("assets/svg/node.svg");
+const CSharp      = asset("assets/svg/csharp.svg");
+const MySQL       = asset("assets/svg/mysql.svg");
+const PostgreSQL  = asset("assets/svg/postgresql.svg");
+const MongoDB     = asset("assets/svg/mongodb.svg");
 
 const BackSkills = () => {
   return (
     <>
-    <div className='skill-card mt-2' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={PHP} alt="" className='w-12 2xl:w-20'/>
-        <div className='text-center font-montserrat'>
-            <h1 className='text-lg font-semibold 2xl:text-2xl'>PHP</h1>
+      <div className="skill-card mt-2" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={PHP} alt="PHP" className="w-12 2xl:w-20" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h1 className="text-lg font-semibold 2xl:text-2xl">PHP</h1>
         </div>
-    </div>
-    <div className='skill-card' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={Node} alt="" className='w-10 2xl:w-16'/>
-        <div className='text-center font-montserrat'>
-            <h1 className='text-lg font-semibold 2xl:text-2xl'>Node.js</h1>
-        </div>
-    </div>
-    <div className='skill-card' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={csharp} alt="" className='w-10 2xl:w-16'/>
-        <div className='text-center font-montserrat'>
-            <h1 className='text-lg font-semibold 2xl:text-2xl'>C#/ASP.NET Core MVC</h1>
-        </div>
-    </div>
-    <div className='skill-card' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={MySQL} alt="" className='w-16 2xl:w-28'/>
-        <div className='text-center font-montserrat'>
-            <h1 className='text-lg font-semibold 2xl:text-2xl'>MySQL</h1>
-        </div>
-    </div>
-    <div className='skill-card' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={PostgreSQL} alt="" className='w-10 2xl:w-16'/>
-        <div className='text-center font-montserrat'>
-            <h1 className='text-lg font-semibold 2xl:text-2xl'>PostgreSQL</h1>
-        </div>
-    </div>
-    <div className='skill-card' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={MongoDB} alt="" className='w-20 2xl:w-32'/>
-        <div className='text-center font-montserrat'>
-            <h1 className='text-lg font-semibold 2xl:text-2xl'>MongoDB</h1>
-        </div>
-    </div>
-    </>
-  )
-}
+      </div>
 
-export default BackSkills
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={Node} alt="Node.js" className="w-10 2xl:w-16" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h1 className="text-lg font-semibold 2xl:text-2xl">Node.js</h1>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={CSharp} alt="C# / ASP.NET Core MVC" className="w-10 2xl:w-16" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h1 className="text-lg font-semibold 2xl:text-2xl">C#/ASP.NET Core MVC</h1>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={MySQL} alt="MySQL" className="w-16 2xl:w-28" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h1 className="text-lg font-semibold 2xl:text-2xl">MySQL</h1>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={PostgreSQL} alt="PostgreSQL" className="w-10 2xl:w-16" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h1 className="text-lg font-semibold 2xl:text-2xl">PostgreSQL</h1>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={MongoDB} alt="MongoDB" className="w-20 2xl:w-32" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h1 className="text-lg font-semibold 2xl:text-2xl">MongoDB</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BackSkills;
--- src/components/skills/parts/FrontSkills.jsx
+++ src/components/skills/parts/FrontSkills.jsx
@@ -1,58 +1,58 @@
-import React from 'react'
-import HTML from 'assets/svg/html.svg'
-import CSS from 'assets/svg/css.svg'
-import Javascript from 'assets/svg/javascript.svg'
-import Bootstrap from 'assets/svg/bootstrap.svg'
-import Tailwind from 'assets/svg/tailwind.svg'
-import Reactjs from 'assets/svg/react.svg'
+import React from "react";
+import { asset } from "../../../lib/asset"; 
+const HTML       = asset("assets/svg/html.svg");
+const CSS        = asset("assets/svg/css.svg");
+const Javascript = asset("assets/svg/javascript.svg");
+const Bootstrap  = asset("assets/svg/bootstrap.svg");
+const Tailwind   = asset("assets/svg/tailwind.svg");
+const Reactjs    = asset("assets/svg/react.svg");
 
 const FrontSkills = () => {
   return (
     <>
-    <div className='skill-card mt-2' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={HTML} alt="" className='w-14 2xl:w-20'/>
-        <div className='text-center font-montserrat'>
-            <h3 className='text-lg font-semibold 2xl:text-2xl'>HTML</h3>
-            
+      <div className="skill-card mt-2" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={HTML} alt="HTML" className="w-14 2xl:w-20" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h3 className="text-lg font-semibold 2xl:text-2xl">HTML</h3>
         </div>
-    </div>
-    <div className='skill-card'  data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={CSS} alt="" className='w-14 2xl:w-20'/>
-        <div className='text-center font-montserrat'>
-            <h3 className='text-lg font-semibold 2xl:text-2xl'>CSS</h3>
-            
-        </div>
-    </div>
-    <div className='skill-card'  data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={Javascript} alt="" className='w-10 2xl:w-16'/>
-        <div className='text-center font-montserrat'>
-            <h3 className='text-lg font-semibold 2xl:text-2xl'>JavaScript</h3>
-            
-        </div>
-    </div>
-    <div className='skill-card'  data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={Bootstrap} alt="" className='w-10 2xl:w-16'/>
-        <div className='text-center font-montserrat'>
-            <h3 className='text-lg font-semibold 2xl:text-2xl'>Bootstrap</h3>
-            
-        </div>
-    </div>
-    <div className='skill-card'  data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={Tailwind} alt="" className='w-10 2xl:w-16'/>
-        <div className='text-center font-montserrat'>
-            <h3 className='text-lg font-semibold 2xl:text-2xl'>Tailwind</h3>
-            
-        </div>
-    </div>
-    <div className='skill-card'  data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-        <img src={Reactjs} alt="" className='w-8 sm:w-10 2xl:w-16'/>
-        <div className='text-center font-montserrat'>
-            <h3 className='text-lg font-semibold 2xl:text-2xl'>React.js/React Native</h3>
-            
-        </div>
-    </div>
-    </>
-  )
-}
+      </div>
 
-export default FrontSkills
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={CSS} alt="CSS" className="w-14 2xl:w-20" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h3 className="text-lg font-semibold 2xl:text-2xl">CSS</h3>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={Javascript} alt="JavaScript" className="w-10 2xl:w-16" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h3 className="text-lg font-semibold 2xl:text-2xl">JavaScript</h3>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={Bootstrap} alt="Bootstrap" className="w-10 2xl:w-16" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h3 className="text-lg font-semibold 2xl:text-2xl">Bootstrap</h3>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={Tailwind} alt="TailwindCSS" className="w-10 2xl:w-16" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h3 className="text-lg font-semibold 2xl:text-2xl">Tailwind</h3>
+        </div>
+      </div>
+
+      <div className="skill-card" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-once="true" data-aos-duration="1000">
+        <img src={Reactjs} alt="React / React Native" className="w-8 sm:w-10 2xl:w-16" loading="lazy" decoding="async" />
+        <div className="text-center font-montserrat">
+          <h3 className="text-lg font-semibold 2xl:text-2xl">React.js/React Native</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FrontSkills;
